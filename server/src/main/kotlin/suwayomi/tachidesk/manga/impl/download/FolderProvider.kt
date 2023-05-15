e01ebfc8b65fadb255eb54c885f5d833b9f6342f
Ben Swanson <bjsswanson@gmail.com>
2023-05-15 21:22:15 +0100

    Update FolderProvider.kt

--- server/src/main/kotlin/suwayomi/tachidesk/manga/impl/download/FolderProvider.kt
+++ server/src/main/kotlin/suwayomi/tachidesk/manga/impl/download/FolderProvider.kt
@@ -1,6 +1,12 @@
 package suwayomi.tachidesk.manga.impl.download
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.sample
 import suwayomi.tachidesk.manga.impl.Page
 import suwayomi.tachidesk.manga.impl.Page.getPageName
 import suwayomi.tachidesk.manga.impl.download.model.DownloadChapter
@@ -22,10 +28,6 @@ class FolderProvider(mangaId: Int, chapterId: Int) : DownloadedFilesProvider(man
         val fileType = file!!.name.substringAfterLast(".")
         return Pair(FileInputStream(file).buffered(), "image/$fileType")
     }
-    
-    fun <A>Collection<A>.forEachParallel(f: suspend (A) -> Unit): Unit = runBlocking {
-        map { async(CommonPool) { f(it) } }.forEach { it.await() }
-    }
 
     @OptIn(FlowPreview::class)
     override suspend fun download(
@@ -38,40 +40,35 @@ class FolderProvider(mangaId: Int, chapterId: Int) : DownloadedFilesProvider(man
         val folder = File(chapterDir)
         folder.mkdirs()
 
-        val range = 0 until pageCount;
-        val chunks = range.chunked(5);
-        
-        for(chunk in chunks) {
-            chunk.forEachParallel { pageNum -> 
-                var pageProgressJob: Job? = null
-                val fileName = getPageName(pageNum) // might have to change this to index stored in database
-                if (isExistingFile(folder, fileName)) continue
-                try {
-                    Page.getPageImage(
-                        mangaId = download.mangaId,
-                        chapterIndex = download.chapterIndex,
-                        index = pageNum
-                    ) { flow ->
-                        pageProgressJob = flow
-                            .sample(100)
-                            .distinctUntilChanged()
-                            .onEach {
-                                download.progress = (pageNum.toFloat() + (it.toFloat() * 0.01f)) / pageCount
-                                step(null, false) // don't throw on canceled download here since we can't do anything
-                            }
-                            .launchIn(scope)
-                    }.first.use { image ->
-                        val filePath = "$chapterDir/$fileName"
-                        ImageResponse.saveImage(filePath, image)
-                    }
-                } finally {
-                    // always cancel the page progress job even if it throws an exception to avoid memory leaks
-                    pageProgressJob?.cancel()
+        for (pageNum in 0 until pageCount) {
+            var pageProgressJob: Job? = null
+            val fileName = getPageName(pageNum) // might have to change this to index stored in database
+            if (isExistingFile(folder, fileName)) continue
+            try {
+                Page.getPageImage(
+                    mangaId = download.mangaId,
+                    chapterIndex = download.chapterIndex,
+                    index = pageNum
+                ) { flow ->
+                    pageProgressJob = flow
+                        .sample(100)
+                        .distinctUntilChanged()
+                        .onEach {
+                            download.progress = (pageNum.toFloat() + (it.toFloat() * 0.01f)) / pageCount
+                            step(null, false) // don't throw on canceled download here since we can't do anything
+                        }
+                        .launchIn(scope)
+                }.first.use { image ->
+                    val filePath = "$chapterDir/$fileName"
+                    ImageResponse.saveImage(filePath, image)
                 }
-                // TODO: retry on error with 2,4,8 seconds of wait
-                download.progress = ((pageNum + 1).toFloat()) / pageCount
-                step(download, false)
+            } finally {
+                // always cancel the page progress job even if it throws an exception to avoid memory leaks
+                pageProgressJob?.cancel()
             }
+            // TODO: retry on error with 2,4,8 seconds of wait
+            download.progress = ((pageNum + 1).toFloat()) / pageCount
+            step(download, false)
         }
         return true
     }
